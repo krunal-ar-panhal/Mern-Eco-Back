4717fa76c503e693e99d1abeae5de2ccedddf206
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-10-17 17:14:17 +0530

    setuo kiya backend ka kuch dependcies bhi install ki gai hain

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello ji");
+});
